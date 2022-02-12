8f42ceb0d9b983f57f127d3da5f114cfaa8b7517
Сергей Назаров <nazaroff.serezha2014@gmail.com>
2022-02-12 12:19:02 +0300

    Добавьте файлы проекта.

---
+++ JsonApp1/Model/ColorsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonApp1.Model
+{
+    class ColorsModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Year { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ JsonApp1/ViewModel/ColorViewModel.cs
@@ -0,0 +1,85 @@
+﻿using JsonApp1.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+public class ColorsViewModel : INotifyPropertyChanged
+{
+    private string name;
+    private string year;
+    private string color;
+
+    public string Name
+    {
+        get { return name; }
+        private set
+        {
+            name = value;
+            OnPropertyChanged("Name");
+        }
+    }
+
+    public string Year
+    {
+        get { return year; }
+        private set
+        {
+            year = value;
+            OnPropertyChanged("Year");
+        }
+    }
+    public string Color
+    {
+        get { return color; }
+        private set
+        {
+            color = value;
+            OnPropertyChanged("color");
+        }
+    }
+
+    public ICommand LoadDataCommand { protected set; get; }
+
+    public ColorsViewModel()
+    {
+        this.LoadDataCommand = new Command(LoadData);
+    }
+
+    private async void LoadData()
+    {
+        string url = "https://reqres.in/api/products/4";
+
+        try
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            var response = await client.GetAsync(client.BaseAddress);
+            response.EnsureSuccessStatusCode(); // выброс исключения, если произошла ошибка
+
+            // десериализация ответа в формате json
+            var content = await response.Content.ReadAsStringAsync();
+            JObject o = JObject.Parse(content);
+
+            var str = o.SelectToken(@"$.data");
+            var ColorsInfo = JsonConvert.DeserializeObject<ColorsModel>(str.ToString());
+
+            this.Name = ColorsInfo.Name;
+            this.Year = ColorsInfo.Year;
+            this.Color = ColorsInfo.Color;
+        }
+        catch (Exception ex)
+        { }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged(string prop = "")
+    {
+        if (PropertyChanged != null)
+            PropertyChanged(this, new PropertyChangedEventArgs(prop));
+    }
+}
+
